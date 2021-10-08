962942ae264ffde749ec15c10b7fdff21de977d0
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2021-10-08 21:47:05 +0200

    fetching done

---
+++ spotify/src/components/Card.jsx
@@ -0,0 +1,43 @@
+const Card = ({ artistData, album }) => {
+  return (
+    <>
+      {artistData.map((song) => (
+        <div
+          className="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-2"
+          key={song.id}
+        >
+          <div className="card pt-2 rp-card">
+            <img
+              src={song.album.cover}
+              alt="..."
+              className="img-fluid h-100 w-100"
+            />
+            <div className="card-body card-text-paragraph">
+              <p className="pb-0 mb-1">{song.album.title}</p>
+              {/* <small>{song.album.cover}</small> */}
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Card;
+
+// return ({
+//   artistData.map((albums) => <h1>{albums.title}</h1>);
+// })
+// <div className="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-2">
+//         <div className="card pt-2 rp-card">
+//           <img
+//             className="recent-ply-card-img px-2"
+//             src={img}
+//             alt="Card image cap"
+//           />
+//           <div className="card-body card-text-paragraph">
+//             <p className="pb-0 mb-1">Queen</p>
+//             <small>Bohemian Rhapsody</small>
+//           </div>
+//         </div>
+//       </div>
